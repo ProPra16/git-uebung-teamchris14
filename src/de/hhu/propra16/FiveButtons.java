134235e1d9d6172b8648f4afb72ee18350e05573
Lukas <volmerich.lukas@hhu.de>
2016-06-14 15:45:16 +0200

    setOnAction

--- src/de/hhu/propra16/FiveButtons.java
+++ src/de/hhu/propra16/FiveButtons.java
@@ -1,9 +1,13 @@
 package de.hhu.propra16;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
@@ -18,7 +22,17 @@ public class FiveButtons extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = (Parent) FXMLLoader.load(getClass().getResource("/five_buttons" +
                 ".fxml"));
-
+        final FileChooser chooser = new FileChooser();
+        final Button btnb = new Button("b");
+        btnb.setOnAction(new EventHandler<ActionEvent>() {
+			
+			public void handle(ActionEvent arg0) {
+				chooser.setTitle("FileChooser");
+			}
+		});
+        btnb.setOnAction(
+        		e -> chooser.setTitle("FileChooser")
+        		);
         primaryStage.setTitle("Five Buttons");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
